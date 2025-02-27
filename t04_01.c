979189d9831a3e6b7f9c15f6940d82dda65bbe9a
Julius Sinaga <143679205+JuliusSinaga@users.noreply.github.com>
2025-02-27 14:00:03 +0700

    Update

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,34 @@
-// NIM - Name
-// NIM - Name
+// 12S23035 - Julius Kaisar Bona Theo Sinaga
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  // Declare variables
+  char numerik[60], keluaran[20];
+  int i;
+
+  // Input
+  for (i = 0; i < 60; i++) {
+    scanf("%c", &numerik[i]);
+    if (numerik[i] == '\n') {
+      numerik[i] = '\0';
+      break;
+    } else {
+    }
+  }
+
+  // Process
+  for (i = 0; i < strlen(numerik); i+= 3) {
+    // Konversi karakter tiga angka ke integer
+    sscanf(numerik + i, "%3hhd", &keluaran[i / 3]);   
+    
+    // Konversi integer ke karakter
+    sprintf(keluaran + i / 3, "%c", keluaran[i / 3]);
+  }
+
+  // Output
+  printf("%s\n", keluaran);
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,31 @@
-// NIM - Name
-// NIM - Name
+// 12S23035 - Julius Kaisar Bona Theo Sinaga
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  // Declare Variables
+  char karakter[20], numerik[60];
+  int i;
+
+  // Input
+  for (i = 0; i < 20; i++) {
+    scanf("%c", &karakter[i]);
+    if (karakter[i] == '\n') {
+      karakter[i] = '\0';
+      break;
+    } else {
+    }
+  }
+
+  // Process
+  for (i = 0; i < strlen(karakter); i++) {
+    // Konversi karakter ke integer
+    sprintf(numerik + i * 3, "%03d", karakter[i]);
+  }
+  
+  // Output
+  printf("%s\n", numerik);
 
   return 0;
 }
